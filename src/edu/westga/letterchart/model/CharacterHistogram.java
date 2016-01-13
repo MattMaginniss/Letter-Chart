155d1d74949c43f5b0fc2515839703e032458f4a
Nbeam <nbeam2@my.westga.edu>
2016-01-13 16:37:43 -0500

    Did the whole thing in one commit as usual

--- src/edu/westga/letterchart/model/CharacterHistogram.java
+++ src/edu/westga/letterchart/model/CharacterHistogram.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.PieChart;
 
@@ -15,36 +16,36 @@ import javafx.scene.chart.PieChart;
  *
  */
 public class CharacterHistogram {
-	
+
 	/**
 	 * Creates an empty CharacterHistogram.
 	 */
 	public CharacterHistogram() {
 
 	}
-	
+
 	/**
 	 * Gets the count for a given character.
-	 * 
+	 *
 	 * @param character the character we're interested in
 	 * @return the number of times it appears in the histogram
 	 */
 	public int getCountFor(char character) {
 		return 0;
 	}
-	
+
 	/**
 	 * Increases (by one) the count for the given character. Ignores non-alphabetic characters.
-	 * 
+	 *
 	 * @param character the character for which we should increase the count
 	 */
 	private void increaseCountFor(char character) {
 
 	}
-	
+
 	/**
 	 * Clears this CharacterHistogram and loads in the new character data
-	 * 
+	 *
 	 * @param textFile the file to open
 	 * @throws FileNotFoundException if the file cannot be found
 	 * @throws IOException if an unexpected I/O problem occurs
@@ -53,13 +54,21 @@ public class CharacterHistogram {
 	public void load(File textFile) throws FileNotFoundException, IOException {
 
 	}
-	
+
 	/**
 	 * Data property, suitable for consumption by a PieChart.
-	 * 
+	 *
 	 * @return the histogram as pie char data
 	 */
 	public ObservableList<PieChart.Data> dataProperty() {
-		return null;
+		ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+
+		return pieChartData;
 	}
 }
