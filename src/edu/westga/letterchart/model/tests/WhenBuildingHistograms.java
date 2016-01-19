520c79e87eb73175185b2cd34067ad1664020c5d
Matt Maginniss <maginniss.matt@gmail.com>
2016-01-19 17:36:59 -0500

    Fixed checkstyle errors.

--- src/edu/westga/letterchart/model/tests/WhenBuildingHistograms.java
+++ src/edu/westga/letterchart/model/tests/WhenBuildingHistograms.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
 
 import org.junit.Before;
 import org.junit.Test;
